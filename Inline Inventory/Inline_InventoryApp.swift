5679f14310b830677d1ae172ee322d5ab53cfe35
nboyers <frenttv@gmail.com>
2022-03-08 20:21:32 -0500

    Initial Commit

---
+++ Inline Inventory/Inline_InventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inline_InventoryApp.swift
+//  Inline Inventory
+//
+//  Created by Noah Boyers on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Inline_InventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
