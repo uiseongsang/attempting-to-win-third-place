c2c611230caf779722396ed91100ae42b889f34d
Alex <alex.o'neill@uconn.edu>
2022-03-09 14:07:22 -0700

    Added Sending Images

--- SPC/db_load_inventory.sql
+++ SPC/db_load_inventory.sql
@@ -5,3 +5,5 @@ CREATE TABLE [Inventory] (
 	[ImagePath] NVARCHAR(50) NOT NULL,
 	[Threshold] INTEGER  NOT NULL
 ); 
+
+INSERT INTO Inventory VALUES(1, 'Test', 100, 'Hot Breakfast/Breakfast Sandwiches & Wraps/Bacon, Gouda & Egg Sandwich.png', 5);
