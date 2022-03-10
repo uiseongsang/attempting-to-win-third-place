21af9eb5b1da33a7a3e4cf545c2a2a3b88a6dc5f
nboyers <frenttv@gmail.com>
2022-03-10 17:51:14 -0500

    UI Fixes

--- Inline Inventory/Views/LookupView.swift
+++ Inline Inventory/Views/LookupView.swift
@@ -10,21 +10,42 @@ import SwiftUI
 struct LookupView: View {
     
     @State var searchText = ""
-    @ObservedObject var serverViewModel = Network()
+    @ObservedObject var network = Network()
+    
     var body: some View {
         
         //MARK: Creates the Seach Bar for use and has binding property
         SearchBarView(searchText: $searchText)
             .padding(.top, 40)
-            .background(Color.white)
         
-        
-        //MARK: Lists the items that equal the name / SKU if it is there
-//        List(serverViewModel.items.filter({ searchText.isEmpty ? true : $0.name.contains(searchText) })) { item in
-//            Text(item.name + String(item.id) + String(item.quantity))
-//        }.background(Color.white)
+        List (network.storeProduct) { user in
+            HStack(alignment:.top) {
+                
+                // Text("\(user.Image)")
+                Text("\(user.Name)")
+                    .padding()
+                Spacer()
+                Text("\(user.QTY)")
+                    .padding()
+                
+                Spacer()
+                Text("\(user.Threshold)")
+                    .padding()
+                Spacer()
+            }
+        } .searchable(text: $searchText)
     }
+    //FIXME: Not working on getting the searchable items
+//    var searchResults: [StoreItem] {
+//        if searchText.isEmpty {
+//            return []
+//        } else {
+//            return network.storeProduct.filter(StoreItem.Name)
+//        }
+//    }
+    
 }
+
 
 //TODO: Create a table instead of a list 
 
