d8a9e7effeae76b922b235755130b5667bdcade5
Alex <alex.o'neill@uconn.edu>
2022-03-09 23:34:28 -0700

    JSON Response Fix
    Availability is also included in response now

--- SPC/load_store_db.sql
+++ SPC/load_store_db.sql
@@ -3,10 +3,9 @@ CREATE TABLE [Inventory] (
 	[Name] NVARCHAR(50)  NOT NULL,
 	[Quantity] INTEGER  NOT NULL,
 	[ImagePath] NVARCHAR(50) NOT NULL,
-	[Threshold] INTEGER  NOT NULL
+	[Threshold] INTEGER  NOT NULL,
+	[Available] Boolean NOT NULL
 ); 
-
-/*
 CREATE TABLE [Orders] ( 
 	[OrderID] INTEGER  NOT NULL PRIMARY KEY, 
 	[VendorID] INTEGER  NOT NULL,
@@ -26,46 +25,46 @@ CREATE TABLE [Transactions] (
 INSERT INTO Transactions VALUES(1, 2 , 3 , 'POS', '03-09-22');
 
 INSERT INTO Orders VALUES(1, 65, 10, 50);
-*/
 
-INSERT INTO INVENTORY VALUES(1, 'Bacon, Gouda & Egg Sandwich', 100, 'http://127.0.0.1:5000/image?path=Hot%20Breakfast/Breakfast%20Sandwiches%20&%20Wraps/Bacon,%20Gouda%20&%20Egg%20Sandwich.png', 5);
-INSERT INTO INVENTORY VALUES(2, 'Double-Smoked Bacon, Cheddar & Egg Sandwich', 100, 'http://127.0.0.1:5000/image?path=Hot%20Breakfast/Breakfast%20Sandwiches%20&%20Wraps/Double-Smoked%20Bacon,%20Cheddar%20&%20Egg%20Sandwich.png', 5);
-INSERT INTO INVENTORY VALUES(3, 'Impossible™ Breakfast Sandwich', 100, "http://127.0.0.1:5000/image?path=Hot%20Breakfast/Breakfast%20Sandwiches%20&%20Wraps/Impossible™%20Breakfast%20Sandwich.png", 5);
-INSERT INTO INVENTORY VALUES(4, 'Turkey Bacon, Cheddar & Egg White Sandwich', 100, "http://127.0.0.1:5000/image?path=Hot%20Breakfast/Breakfast%20Sandwiches%20&%20Wraps/Turkey%20Bacon,%20Cheddar%20&%20Egg%20White%20Sandwich.png", 5);
-INSERT INTO INVENTORY VALUES(5, 'Spinach, Feta & Egg White Wrap', 100, "http://127.0.0.1:5000/image?path=Hot%20Breakfast/Breakfast%20Sandwiches%20&%20Wraps/Spinach,%20Feta%20&%20Egg%20White%20Wrap.png", 5);
-INSERT INTO INVENTORY VALUES(6, 'Kale & Mushroom Egg Bites', 100, "http://127.0.0.1:5000/image?path=Hot%20Breakfast/Sous%20Vide%20Egg%20Bites/Kale%20&%20Mushroom%20Egg%20Bites.png", 5);
-INSERT INTO INVENTORY VALUES(7, 'Bacon & Gruyère Egg Bites', 100, "http://127.0.0.1:5000/image?path=Hot%20Breakfast/Sous%20Vide%20Egg%20Bites/Bacon%20&%20Gruyère%20Egg%20Bites.png", 5);
-INSERT INTO INVENTORY VALUES(8, 'Egg White & Roasted Red Pepper Egg Bites', 100, "http://127.0.0.1:5000/image?path=Hot%20Breakfast/Sous%20Vide%20Egg%20Bites/Egg%20White%20&%20Roasted%20Red%20Pepper%20Egg%20Bites.png", 5);
-INSERT INTO INVENTORY VALUES(9, 'Crispy Grilled Cheese on Sourdough', 100, "http://127.0.0.1:5000/image?path=Lunch/Warm%20Sandwiches/Crispy%20Grilled%20Cheese%20on%20Sourdough.png", 5);
-INSERT INTO INVENTORY VALUES(10, 'Ham & Swiss on Baguette', 100, "http://127.0.0.1:5000/image?path=Lunch/Warm%20Sandwiches/Ham%20&%20Swiss%20on%20Baguette.png", 5);
-INSERT INTO INVENTORY VALUES(11, 'Turkey, Provolone & Pesto on Ciabatta', 100, "http://127.0.0.1:5000/image?path=Lunch/Warm%20Sandwiches/Turkey,%20Provolone%20&%20Pesto%20on%20Ciabatta.png", 5);
-INSERT INTO INVENTORY VALUES(12, 'Chicken Caprese on Ciabatta', 100, "http://127.0.0.1:5000/image?path=Lunch/Warm%20Sandwiches/Chicken%20Caprese%20on%20Ciabatta.png", 5);
-INSERT INTO INVENTORY VALUES(13, 'Chicken & Bacon on Brioche', 100, "http://127.0.0.1:5000/image?path=Lunch/Warm%20Sandwiches/Chicken%20&%20Bacon%20on%20Brioche.png", 5);
-INSERT INTO INVENTORY VALUES(14, 'Tomato & Mozzarella on Focaccia', 100, "http://127.0.0.1:5000/image?path=Lunch/Warm%20Sandwiches/Tomato%20&%20Mozzarella%20on%20Focaccia.png", 5);
-INSERT INTO INVENTORY VALUES(15, 'Chickpea Bites & Avocado Protein Box', 100, "http://127.0.0.1:5000/image?path=Lunch/Protein%20Boxes/Chickpea%20Bites%20&%20Avocado%20Protein%20Box.png", 5);
-INSERT INTO INVENTORY VALUES(16, 'Chicken & Hummus Protein Box', 100, "http://127.0.0.1:5000/image?path=Lunch/Protein%20Boxes/Chicken%20&%20Hummus%20Protein%20Box.png", 5);
-INSERT INTO INVENTORY VALUES(17, 'Eggs & Cheddar Protein Box', 100, "http://127.0.0.1:5000/image?path=Lunch/Protein%20Boxes/Eggs%20&%20Cheddar%20Protein%20Box.png", 5);
-INSERT INTO INVENTORY VALUES(18, 'PB&J Protein Box', 100, "http://127.0.0.1:5000/image?path=Lunch/Protein%20Boxes/PB&J%20Protein%20Box.png", 5);
-INSERT INTO INVENTORY VALUES(19, 'Cheese & Fruit Protein Box', 100, "http://127.0.0.1:5000/image?path=Lunch/Protein%20Boxes/Cheese%20&%20Fruit%20Protein%20Box.png", 5);
-INSERT INTO INVENTORY VALUES(20, 'Plain Bagel', 100, "http://127.0.0.1:5000/image?path=Bakery/Bagles/Plain%20Bagel.png", 5);
-INSERT INTO INVENTORY VALUES(21, 'Avocado Spread', 100, "http://127.0.0.1:5000/image?path=Bakery/Bagles/Avocado%20Spread.png", 5);
-INSERT INTO INVENTORY VALUES(22, 'Earth Cake Pop', 100, "http://127.0.0.1:5000/image?path=Bakery/Cake%20Pops/Earth%20Cake%20Pop.png", 5);
-INSERT INTO INVENTORY VALUES(23, 'Birthday Cake Pop', 100, "http://127.0.0.1:5000/image?path=Bakery/Cake%20Pops/Birthday%20Cake%20Pop.png", 5);
-INSERT INTO INVENTORY VALUES(24, 'Chocolate Cake Pop', 100, "http://127.0.0.1:5000/image?path=Bakery/Cake%20Pops/Chocolate%20Cake%20Pop.png", 5);
-INSERT INTO INVENTORY VALUES(25, 'Double Chocolate Brownie', 100, "http://127.0.0.1:5000/image?path=Bakery/Cookies,%20Brownies%20&%20Bars/Double%20Chocolate%20Brownie.png", 5);
-INSERT INTO INVENTORY VALUES(26, 'Chocolate Chip Cookie', 100, "http://127.0.0.1:5000/image?path=Bakery/Cookies,%20Brownies%20&%20Bars/Chocolate%20Chip%20Cookie.png", 5);
-INSERT INTO INVENTORY VALUES(27, 'Marshmallow Dream Bar', 100, "http://127.0.0.1:5000/image?path=Bakery/Cookies,%20Brownies%20&%20Bars/Marshmallow%20Dream%20Bar.png", 5);
-INSERT INTO INVENTORY VALUES(28, 'Ham & Swiss Croissant', 100, "http://127.0.0.1:5000/image?path=Bakery/Croissants/Ham%20&%20Swiss%20Croissant.png", 5);
-INSERT INTO INVENTORY VALUES(29, 'Butter Croissant', 100, "http://127.0.0.1:5000/image?path=Bakery/Croissants/Butter%20Croissant.png", 5);
-INSERT INTO INVENTORY VALUES(30, 'Almond Croissant', 100, "http://127.0.0.1:5000/image?path=Bakery/Croissants/Almond%20Croissant.png", 5);
-INSERT INTO INVENTORY VALUES(31, 'Chocolate Croissant', 100, "http://127.0.0.1:5000/image?path=Bakery/Croissants/Chocolate%20Croissant.png", 5);
-INSERT INTO INVENTORY VALUES(32, 'Red Velvet Loaf', 100, "http://127.0.0.1:5000/image?path=Bakery/Loaves%20&%20Cakes/Red%20Velvet%20Loaf.png", 5);
-INSERT INTO INVENTORY VALUES(33, 'Cinnamon Coffee Cake', 100, "http://127.0.0.1:5000/image?path=Bakery/Loaves%20&%20Cakes/Cinnamon%20Coffee%20Cake.png", 5);
-INSERT INTO INVENTORY VALUES(34, 'Iced Lemon Loaf', 100, "http://127.0.0.1:5000/image?path=Bakery/Loaves%20&%20Cakes/Iced%20Lemon%20Loaf.png", 5);
-INSERT INTO INVENTORY VALUES(35, 'Pumpkin & Pepita Loaf', 100, "http://127.0.0.1:5000/image?path=Bakery/Loaves%20&%20Cakes/Pumpkin%20&%20Pepita%20Loaf.png", 5);
-INSERT INTO INVENTORY VALUES(36, 'Banana Walnut & Pecan Loaf', 100, "http://127.0.0.1:5000/image?path=Bakery/Loaves%20&%20Cakes/Banana%20Walnut%20&%20Pecan%20Loaf.png", 5);
-INSERT INTO INVENTORY VALUES(37, 'Cheese Danish', 100, "http://127.0.0.1:5000/image?path=Bakery/Danishes%20&%20Doughnuts/Cheese%20Danish.png", 5);
-INSERT INTO INVENTORY VALUES(38, 'Glazed Doughnut', 100, "http://127.0.0.1:5000/image?path=Bakery/Danishes%20&%20Doughnuts/Glazed%20Doughnut.png", 5);
-INSERT INTO INVENTORY VALUES(39, 'Blueberry Scone', 100, "http://127.0.0.1:5000/image?path=Bakery/Muffins%20&%20Scones/Blueberry%20Scone.png", 5);
-INSERT INTO INVENTORY VALUES(40, 'Petite Vanilla Bean Scone', 100, "http://127.0.0.1:5000/image?path=Bakery/Muffins%20&%20Scones/Petite%20Vanilla%20Bean%20Scone.png", 5);
-INSERT INTO INVENTORY VALUES(41, 'Blueberry Muffin', 100, "http://127.0.0.1:5000/image?path=Bakery/Muffins%20&%20Scones/Blueberry%20Muffin.png", 5);
+
+INSERT INTO Inventory VALUES(1, 'Bacon, Gouda & Egg Sandwich', 100, 'Hot Breakfast/Breakfast Sandwiches & Wraps/Bacon, Gouda & Egg Sandwich.png',5, True);
+INSERT INTO Inventory VALUES(2, 'Double-Smoked Bacon, Cheddar & Egg Sandwich', 100, 'Hot Breakfast/Breakfast Sandwiches & Wraps/Double-Smoked Bacon, Cheddar & Egg Sandwich.png',5, True);
+INSERT INTO INVENTORY VALUES(3, 'Impossible™ Breakfast Sandwich', 100, "Hot Breakfast/Breakfast Sandwiches & Wraps/Impossible™ Breakfast Sandwich.png",5, True);
+INSERT INTO INVENTORY VALUES(4, 'Turkey Bacon, Cheddar & Egg White Sandwich', 100, "Hot Breakfast/Breakfast Sandwiches & Wraps/Turkey Bacon, Cheddar & Egg White Sandwich.png",5, True);
+INSERT INTO INVENTORY VALUES(5, 'Spinach, Feta & Egg White Wrap', 100, "Hot Breakfast/Breakfast Sandwiches & Wraps/Spinach, Feta & Egg White Wrap.png",5, True);
+INSERT INTO INVENTORY VALUES(6, 'Kale & Mushroom Egg Bites', 100, "Hot Breakfast/Sous Vide Egg Bites/Kale & Mushroom Egg Bites.png",5, True);
+INSERT INTO INVENTORY VALUES(7, 'Bacon & Gruyère Egg Bites', 100, "Hot Breakfast/Sous Vide Egg Bites/Bacon & Gruyère Egg Bites.png",5, True);
+INSERT INTO INVENTORY VALUES(8, 'Egg White & Roasted Red Pepper Egg Bites', 100, "Hot Breakfast/Sous Vide Egg Bites/Egg White & Roasted Red Pepper Egg Bites.png",5, True);
+INSERT INTO INVENTORY VALUES(9, 'Crispy Grilled Cheese on Sourdough', 100, "Lunch/Warm Sandwiches/Crispy Grilled Cheese on Sourdough.png",5, True);
+INSERT INTO INVENTORY VALUES(10, 'Ham & Swiss on Baguette', 100, "Lunch/Warm Sandwiches/Ham & Swiss on Baguette.png",5, True);
+INSERT INTO INVENTORY VALUES(11, 'Turkey, Provolone & Pesto on Ciabatta', 100, "Lunch/Warm Sandwiches/Turkey, Provolone & Pesto on Ciabatta.png",5, True);
+INSERT INTO INVENTORY VALUES(12, 'Chicken Caprese on Ciabatta', 100, "Lunch/Warm Sandwiches/Chicken Caprese on Ciabatta.png",5, True);
+INSERT INTO INVENTORY VALUES(13, 'Chicken & Bacon on Brioche', 100, "Lunch/Warm Sandwiches/Chicken & Bacon on Brioche.png",5, True);
+INSERT INTO INVENTORY VALUES(14, 'Tomato & Mozzarella on Focaccia', 100, "Lunch/Warm Sandwiches/Tomato & Mozzarella on Focaccia.png",5, True);
+INSERT INTO INVENTORY VALUES(15, 'Chickpea Bites & Avocado Protein Box', 100, "Lunch/Protein Boxes/Chickpea Bites & Avocado Protein Box.png",5, True);
+INSERT INTO INVENTORY VALUES(16, 'Chicken & Hummus Protein Box', 100, "Lunch/Protein Boxes/Chicken & Hummus Protein Box.png",5, True);
+INSERT INTO INVENTORY VALUES(17, 'Eggs & Cheddar Protein Box', 100, "Lunch/Protein Boxes/Eggs & Cheddar Protein Box.png",5, True);
+INSERT INTO INVENTORY VALUES(18, 'PB&J Protein Box', 100, "Lunch/Protein Boxes/PB&J Protein Box.png",5, True);
+INSERT INTO INVENTORY VALUES(19, 'Cheese & Fruit Protein Box', 100, "Lunch/Protein Boxes/Cheese & Fruit Protein Box.png",5, True);
+INSERT INTO INVENTORY VALUES(20, 'Plain Bagel', 100, "Bakery/Bagles/Plain Bagel.png",5, True);
+INSERT INTO INVENTORY VALUES(21, 'Avocado Spread', 100, "Bakery/Bagles/Avocado Spread.png",5, True);
+INSERT INTO INVENTORY VALUES(22, 'Earth Cake Pop', 100, "Bakery/Cake Pops/Earth Cake Pop.png",5, True);
+INSERT INTO INVENTORY VALUES(23, 'Birthday Cake Pop', 100, "Bakery/Cake Pops/Birthday Cake Pop.png",5, True);
+INSERT INTO INVENTORY VALUES(24, 'Chocolate Cake Pop', 100, "Bakery/Cake Pops/Chocolate Cake Pop.png",5, True);
+INSERT INTO INVENTORY VALUES(25, 'Double Chocolate Brownie', 100, "Bakery/Cookies, Brownies & Bars/Double Chocolate Brownie.png",5, True);
+INSERT INTO INVENTORY VALUES(26, 'Chocolate Chip Cookie', 100, "Bakery/Cookies, Brownies & Bars/Chocolate Chip Cookie.png",5, True);
+INSERT INTO INVENTORY VALUES(27, 'Marshmallow Dream Bar', 100, "Bakery/Cookies, Brownies & Bars/Marshmallow Dream Bar.png",5, True);
+INSERT INTO INVENTORY VALUES(28, 'Ham & Swiss Croissant', 100, "Bakery/Croissants/Ham & Swiss Croissant.png",5, True);
+INSERT INTO INVENTORY VALUES(29, 'Butter Croissant', 100, "Bakery/Croissants/Butter Croissant.png",5, True);
+INSERT INTO INVENTORY VALUES(30, 'Almond Croissant', 100, "Bakery/Croissants/Almond Croissant.png",5, True);
+INSERT INTO INVENTORY VALUES(31, 'Chocolate Croissant', 100, "Bakery/Croissants/Chocolate Croissant.png",5, True);
+INSERT INTO INVENTORY VALUES(32, 'Red Velvet Loaf', 100, "Bakery/Loaves & Cakes/Red Velvet Loaf.png",5, True);
+INSERT INTO INVENTORY VALUES(33, 'Cinnamon Coffee Cake', 100, "Bakery/Loaves & Cakes/Cinnamon Coffee Cake.png",5, True);
+INSERT INTO INVENTORY VALUES(34, 'Iced Lemon Loaf', 100, "Bakery/Loaves & Cakes/Iced Lemon Loaf.png",5, True);
+INSERT INTO INVENTORY VALUES(35, 'Pumpkin & Pepita Loaf', 100, "Bakery/Loaves & Cakes/Pumpkin & Pepita Loaf.png",5, True);
+INSERT INTO INVENTORY VALUES(36, 'Banana Walnut & Pecan Loaf', 100, "Bakery/Loaves & Cakes/Banana Walnut & Pecan Loaf.png",5, True);
+INSERT INTO INVENTORY VALUES(37, 'Cheese Danish', 100, "Bakery/Danishes & Doughnuts/Cheese Danish.png",5, True);
+INSERT INTO INVENTORY VALUES(38, 'Glazed Doughnut', 100, "Bakery/Danishes & Doughnuts/Glazed Doughnut.png",5, True);
+INSERT INTO INVENTORY VALUES(39, 'Blueberry Scone', 100, "Bakery/Muffins & Scones/Blueberry Scone.png",5, True);
+INSERT INTO INVENTORY VALUES(40, 'Petite Vanilla Bean Scone', 100, "Bakery/Muffins & Scones/Petite Vanilla Bean Scone.png",5, True);
+INSERT INTO INVENTORY VALUES(41, 'Blueberry Muffin', 100, "Bakery/Muffins & Scones/Blueberry Muffin.png",5, True);
--- SPC/server.py
+++ SPC/server.py
@@ -1,5 +1,5 @@
 # server.py
-from flask import Flask, request, send_file, render_template
+from flask import Flask, request, send_file, render_template, jsonify, Response
 import sqlite3
 import json
 import urllib 
@@ -25,12 +25,13 @@ def get_item():
     id = request.args['sku']  #example: http://127.0.0.1:5000/item?sku=1
     result = c.execute(f"SELECT * FROM Inventory WHERE SKU = {id}").fetchall()[0] #using item id from the get request
     #might need to do something to image
-    return json.dumps({
+    return jsonify({
         "SKU" : result[0],
         "Name": result[1],
         "QTY" : result[2],
         "Image": result[3],
-        "Threshold": result[4]
+        "Threshold": result[4],
+        "Available": result[5]
     })
 
 @app.route("/", methods=["GET"])
@@ -39,15 +40,16 @@ def get_all():
     Get item number from get request and return item info
     """
     #example: http://127.0.0.1:5000/
-    result = c.execute(f"SELECT * FROM Inventory").fetchall() #using item id from the get request
-    results = str(list(map(lambda x: json.dumps({
+    result = c.execute("SELECT * FROM Inventory").fetchall() #using item id from the get request
+    results = list(map(lambda x: {
         "SKU" : x[0],
         "Name": x[1],
         "QTY" : x[2],
         "Image": x[3],
-        "Threshold": x[4]
-    }), result)))
-    return results
+        "Threshold": x[4],
+        "Available": x[5]
+    }, result))
+    return Response(json.dumps(results),  mimetype='application/json')
 
 @app.route("/item", methods=["POST"])
 def add_item():
@@ -56,17 +58,18 @@ def add_item():
     """
     if request.is_json:
         item = request.get_json()
-        last_item = c.execute(f"SELECT SKU FROM Inventory ORDER BY Skew DESC").fetchall()[0][0]
+        last_item = c.execute("SELECT SKU FROM Inventory ORDER BY Skew DESC").fetchall()[0][0]
         # values(sku, name, qty, imagepath, lst)
-        c.execute(f"INSERT INTO Inventory VALUES({last_item+1}, {item['name']}, {item['qty']}, {item['image']}, 5);")
+        c.execute(f"INSERT INTO Inventory VALUES({last_item+1}, {item['name']}, {item['qty']}, {item['image']}, 5, True);")
         result = c.execute(f"SELECT * FROM Inventory WHERE SKU = {last_item+1}").fetchall()[0]
-        return json.dumps({
+        return jsonify(json.dumps({
             "SKU" : result[0],
             "Name": result[1],
             "QTY" : result[2],
             "Image": result[3],
-            "Threshold": result[4]
-            }), 201
+            "Threshold": result[4],
+            "Available": result[5]
+            })), 201
     return {"error": "Request must be JSON"}, 415
 
 @app.route("/item", methods=["PUT"])
@@ -77,15 +80,16 @@ def update_item():
     if request.is_json:
         item = request.get_json()
         # values(sku, name, qty, imagepath, lst)
-        c.execute(f"UPDATE Inventory SET Quantity = {item['qty']} WHERE SKU = {item['sku']};")
+        c.execute(f"UPDATE Inventory SET Quantity = {item['qty']}, Available = {0 if item['qty'] <= 0 else 1} WHERE SKU = {item['sku']};")
         result = c.execute(f"SELECT * FROM Inventory WHERE SKU = {item['sku']}").fetchall()[0]
-        return json.dumps({
+        return jsonify(json.dumps({
             "SKU" : result[0],
             "Name": result[1],
             "QTY" : result[2],
             "Image": result[3],
-            "Threshold": result[4]
-            }), 201
+            "Threshold": result[4],
+            "Available": result[5]
+            })), 201
     return {"error": "Request must be JSON"}, 415
 
 @app.route("/image")
@@ -98,3 +102,17 @@ def get_image():
     path = urllib.parse.unquote(name)
     filename = '../Image/'+path
     return send_file(filename, mimetype='image/gif')
+
+# @app.route("/transactions", methods=["POST"])
+# def remove_item():
+#     """
+#     Add item to transaction and lower qty from inventory
+#     """
+#     if request.is_json:
+#         item = request.get_json()
+#         # [UserID] [SKU] [Quantity] [Order_type] [Date]
+#         c.execute(f"INSERT INTO Transactions VALUES({item['userID']}, {item['sku']}, {item['qty']}, {item['order']}, {item['date']});")
+#         count = c.execute(f"SELECT qty FROM Inventory WHERE SKU = {item['sku']}").fetchall()[0]
+#         c.execute(f"UPDATE Inventory SET Quantity = {count - item['qty']} WHERE SKU = {item['sku']};")
+#         return "Your transaction is complete", 201
+#     return {"error": "Request must be JSON"}, 415
