3f412bcdde2f30692857b94e68af47584cf31c62
kumokuro <katieannemaher@gmail.com>
2022-03-10 12:37:16 -0600

    added querySelectors to app.js

---
+++ SPC/UI/app.js
@@ -0,0 +1,15 @@
+// DOM SELECTORS
+
+// Item Availability
+const mopToggleBtn = document.querySelector("#mop-toggle-btn"); //toggle btn
+const AvailabilityContainer = document.querySelector("#availability-container"); //Availability table
+
+// IMS
+const imsSaveBtn = document.querySelector("#ims-save-btn"); //save btn
+const imsApproveBtn = document.querySelector("#ims-approve-btn"); //approve btn
+const imsContainer = document.querySelector("#ims-container"); //IMS table
+const onHandInput = document.querySelector("#on-hand-input"); //on hand qty
+
+// Merch Lookup
+const merchSearchInput = document.querySelector("#merch-search-input"); //search bar
+const merchSearchBtn = document.querySelector("#merch-search-btn"); //search btn
